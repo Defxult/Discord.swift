4bce6c2aab8bc00b181df1fb7f983d8aa6c81593
Defxult <52070034+Defxult@users.noreply.github.com>
2023-10-08 12:14:34 -0400

    Set Vapor package to "from: 4.84.3"

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.14.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.79.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.84.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
